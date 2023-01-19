3f0f80e098d4210fce0d3e0609a6c7c94221a70f
HamzaAG <119421875+HamzaAG@users.noreply.github.com>
2023-01-19 15:58:55 +0100

    Update reducer.js

--- src/Config2/reducer.js
+++ src/Config2/reducer.js
@@ -6,7 +6,18 @@ const initialState = {
         { id: 4, name: "Ayoub MOUSSTAKIM", email: "mousstakim@gmail.com" },
         { id: 5, name: "Mohamed BERBOUCHI", email: "berbouchi@gmail.com" },
         { id: 6, name: "Mehdi MAGHLAL", email: "maghlal@gmail.com" },
-        { id: 7, name: "Abdelfatah ARSSAOUI", email: "arssaoui@gmail.com" }
+        { id: 7, name: "Abdelfatah ARSSAOUI", email: "arssaoui@gmail.com" },
+        { id: 8, name: "Ilyas AITBOUDOUAR", email: "aitboudouar@gmail.com" },
+        { id: 9, name: "Marouan MOURAD", email: "mourad@gmail.com" },
+        { id: 10, name: "Adnane ABDELLAOUI", email: "abdellaoui@gmail.com" },
+        { id: 11, name: "Abdellah OUACHOUCH", email: "ouachouch@gmail.com" },
+        { id: 12, name: "Khalil LAKHNIKH", email: "lakhnikh@gmail.com" },
+        { id: 13, name: "Hamza ANOUAR", email: "anouar@gmail.com" },
+        { id: 14, name: "Oussama CHERQUAOUI", email: "cherquaoui@gmail.com" },
+        { id: 15, name: "Walid OUBOUH", email: "oubouh@gmail.com" },
+        { id: 16, name: "Anass AITTAMLDO", email: "aittanldo@gmail.com" },
+        { id: 17, name: "Zakaria BAHID", email: "bahid@gmail.com" },
+        { id: 18, name: "Zakaria LAHGAZ", email: "lahgaz@gmail.com" }
     ] 
 };
 const reducer = (state=initialState, action) => {
